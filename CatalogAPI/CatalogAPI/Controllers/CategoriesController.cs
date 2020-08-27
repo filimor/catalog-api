862bcae6fc76632bf148e79a7869fc09a461d18a
Filipe Moreira <filimor@posteo.com.br>
2020-08-27 14:18:05 -0300

    Implement Categories controller

---
+++ CatalogAPI/CatalogAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,69 @@
+﻿using CatalogAPI.Context;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+            return _context.Categories.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetCategory")]
+        public ActionResult<Category> Get(int id)
+        {
+            var category = _context.Categories.AsNoTracking().FirstOrDefault(c => c.Id == id);
+            return category ?? (ActionResult<Category>)NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<Category> Post([FromBody] Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("GetCategory", new { id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Category> Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return category;
+        }
+    }
+}
