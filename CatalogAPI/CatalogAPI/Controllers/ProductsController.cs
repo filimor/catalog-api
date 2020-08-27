0ef09e46aa766dcfc3e140e539cd8ea491a45914
Filipe Moreira <filimor@posteo.com.br>
2020-08-27 13:17:59 -0300

    Implement Products controller

---
+++ CatalogAPI/CatalogAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatalogAPI.Context;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
